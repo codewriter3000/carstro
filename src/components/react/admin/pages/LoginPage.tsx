08458ceb42f0cf532b667fb3c4ae2ae1948d2ba9
Alex Micharski <amicharski@outlook.com>
2024-05-21 13:36:54 -0400

    added login form

--- src/components/react/admin/pages/LoginPage.tsx
+++ src/components/react/admin/pages/LoginPage.tsx
@@ -1,14 +1,23 @@
-import {Form, Grid, TextInput, Tile} from '@carbon/react'
+import {Button, Form, FlexGrid, TextInput, Theme, Tile, PasswordInput, Stack} from '@carbon/react'
+import React from 'react'
 
 const LoginPage = () => {
     return (
-        <Grid className='login-page' fullWidth>
-            <Tile>
-                <Form>
-                    <TextInput labelText='Email' id='email' />
-                </Form>
-            </Tile>
-        </Grid>
+        <Theme theme='g100' className='login-page'>
+            <div className='login-frame'>
+                <FlexGrid>
+                    <Tile>
+                        <Form className='login-form'>
+                            <Stack gap={7}>
+                                <TextInput labelText='Username' id='username' />
+                                <PasswordInput labelText='Password' id='password' />
+                                <Button>Login</Button>
+                            </Stack>
+                        </Form>
+                    </Tile>
+                </FlexGrid>
+            </div>
+        </Theme>
     )
 }
 
