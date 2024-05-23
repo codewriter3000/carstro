7fa82c3634daf71a85555227f5e53404d5b1e768
Alex Micharski <amicharski@outlook.com>
2024-05-23 17:08:30 -0400

    fixed font errors and styled modal

--- src/components/react/admin/components/ConfigureUserModal.jsx
+++ src/components/react/admin/components/ConfigureUserModal.jsx
@@ -1,8 +1,23 @@
-import {Button, Dropdown, MultiSelect, Select, SelectItem, TextInput} from "@carbon/react";
+import {
+    Accordion,
+    AccordionItem,
+    Button,
+    Dropdown,
+    MultiSelect,
+    Select,
+    SelectItem, Stack,
+    TextInput,
+    Toggle
+} from '@carbon/react'
+import {useState} from "react";
 
 const ConfigureUserModal = ({ user }) => {
+    const [isAdmin, setIsAdmin] = useState(user?.['admin'])
+    const [isDisabled, setIsDisabled] = useState(user?.['isDisabled'])
+    const [deleteStage, setDeleteStage] = useState('Delete Account')
+
     return (
-        <>
+        <Stack gap={7}>
             <p style={{
                 marginBottom: '1rem'
             }}>
@@ -19,11 +34,43 @@ const ConfigureUserModal = ({ user }) => {
                        defaultValue={user?.['last_name']}
 
             />
-            {user?.['is_admin'] ? <Button kind='danger--ghost'>Demote from Staff</Button>
-            : <Button kind='tertiary'>Promote to Staff</Button>}
-            <Button kind='danger--tertiary'>Disable Account</Button>
-            <Button kind='danger'>Delete Account</Button>
-        </>
+            <div className='grid grid-cols-2'>
+                <div>
+                    <Toggle labelText='Is admin'
+                            labelA='Standard user'
+                            labelB='Admin user'
+                            toggled={isAdmin}
+                            onClick={() => setIsAdmin(curr => !curr)}
+                    />
+                </div>
+                <div>
+                    <Toggle labelText='Is account disabled'
+                            labelA='Enabled account'
+                            labelB='Disabled account'
+                            toggled={isDisabled}
+                            onClick={(tog) => {
+                                setIsDisabled(curr => !curr)
+                                console.log(tog)
+                            }}
+                    />
+                </div>
+            </div>
+            <Accordion>
+                <AccordionItem title='Delete Account'>
+                    <div className='flex'>
+                        <div>
+                            <Button kind='danger'
+                                    onClick={() => setDeleteStage('Confirm Delete')}>
+                                {deleteStage}
+                            </Button>
+                        </div>
+                        {deleteStage === 'Confirm Delete' && <div className='m-auto'>
+                            Are you sure you want to delete this account?
+                        </div>}
+                    </div>
+                </AccordionItem>
+            </Accordion>
+        </Stack>
     )
 }
 
